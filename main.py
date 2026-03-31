572a3eec22df6bddd3da56a74822d4a073f25b51
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-03-31 17:24:10 +0200

    Add try except blocks for incorrect path to book

--- main.py
+++ main.py
@@ -18,11 +18,14 @@ def main():
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     path = sys.argv[1]
-    text = get_book_text(path)
-    num_words = get_num_of_words(text)
-    num_chars = get_num_of_chars(text)
-    list = convert_dict_to_list(num_chars)
-    print_stats(path, num_words, list)
+    try:
+        text = get_book_text(path)
+        num_words = get_num_of_words(text)
+        num_chars = get_num_of_chars(text)
+        list = convert_dict_to_list(num_chars)
+        print_stats(path, num_words, list)
+    except Exception as e:
+        print(e)
 
 
 main()
